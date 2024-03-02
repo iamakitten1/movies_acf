cf980cf1c61791891467e03929cd98eb00baf175
unknown <terbagdasarovartur@gmail.com>
2024-03-02 17:10:13 +0400

    Add slider function

--- testimonial.php
+++ testimonial.php
@@ -36,6 +36,7 @@
   <link href="css/responsive.css" rel="stylesheet" />
   <?php
     require "header.php";
+    require "slides-function.php";
     require "testimonial-function.php";
     require "subscribe-function.php";
     require "adress-section.php";
